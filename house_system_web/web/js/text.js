264364e2b87799fc5c002dcec63dc7f07a1f8d05
xiaoabc4 <508471408@qq.com>
2020-10-30 09:25:43 +0800

    10.230

--- house_system_web/web/js/text.js
+++ house_system_web/web/js/text.js
@@ -1,6 +1,7 @@
- var urlone = 'http://localhost:8080/xy_house_system_war_exploded/dept';
- var urltwo = 'http://localhost:8080/xy_house_system_war_exploded/myjs';
- var urltree = 'http://localhost:8080/xy_house_system_war_exploded/myemp';
+ // var urlone = 'http://localhost:8080/xy_house_system_war_exploded/dept';
+ // var urltwo = 'http://localhost:8080/xy_house_system_war_exploded/myjs';
+ // var urltree = 'http://localhost:8080/xy_house_system_war_exploded/myemp';
 
-// var urlone = 'http://localhost:8088/xy_house_system_war_exploded/dept';
-// var urltwo = 'http://localhost:8088/xy_house_system_war_exploded/myjs';
+var urlone = 'http://localhost:8088/xy_house_system_war_exploded/dept';
+var urltwo = 'http://localhost:8088/xy_house_system_war_exploded/myjs';
+ var urltree = 'http://localhost:8088/xy_house_system_war_exploded/myemp';
